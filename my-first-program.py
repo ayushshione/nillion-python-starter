329739aa87e3823cebf9f55a66458b2378d621db
Ayush Shione <96991889+ayushshione@users.noreply.github.com>
2024-06-27 23:10:03 +0530

    Create my-first-program.py

---
+++ my-first-program.py
@@ -0,0 +1,8 @@
+from nada_dsl import *
+
+def nada_main():
+    party1 = Party(name="Party1")
+    my_int1 = SecretInteger(Input(name="my_int1", party=party1))
+    my_int2 = SecretInteger(Input(name="my_int2", party=party1))
+    result = my_int1 + my_int2
+    return [Output(result, "sum_output", party1)]
